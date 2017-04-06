dae5ac230a0ffa4b750b8501ba38caabf3d83d2c
子勰 <code@bihe0832.com>
2017-04-06 09:20:45 +0800

    添加代码及文档

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
-# getPNGInfo
-一款基于Java环境的获取png图片信息的工具
+## 背景
+
+### APK Signature Scheme v2官方介绍
+
+
+- `TYPE_CUSTOM` = 0
+
+	图像类型无法识别，因此它必须是自定义图像。
+	
+- `TYPE_INT_RGB` = 1
+
+	表示具有8位RGB颜色分量的整数像素的图像，他没有alpha值。
+
+- `TYPE_INT_ARGB` = 2
+
+	表示具有8位RGBA颜色成分的整数像素的图像，她有alpha值。
+	
+- `TYPE_INT_ARGB_PRE` = 3
+
+	表示具有8位RGBA颜色成分的整数像素的图像。
+	
+- `TYPE_INT_BGR` = 4
+
+	表示具有8位RGB颜色分量的图像，对应于Windows或Solaris样式的BGR颜色模型，蓝色，绿色和红色颜色打包成整数像素。
+		
+- `TYPE_3BYTE_BGR` = 5
+
+	表示具有8位RGB颜色分量的图像，对应于Windows风格的BGR颜色模型，蓝色，绿色和红色以3个字节存储。
+
+- `TYPE_4BYTE_ABGR` = 6
+
+	表示具有蓝色，绿色和红色的8位RGBA颜色分量的图像，存储在3个字节和1个字节的alpha中。
+
+- `TYPE_4BYTE_ABGR_PRE` = 7
+
+	表示具有蓝色，绿色和红色的8位RGBA颜色分量的图像，存储在3个字节和1个字节的alpha中。
+
+- `TYPE_USHORT_565_RGB` = 8
+
+	表示具有5-6-5 RGB颜色分量（5位红色，6位绿色，5位蓝色），无alpha的图像。
+
+- `TYPE_USHORT_555_RGB` = 9
+
+	表示5-5-5 RGB颜色分量（5位红色，5位绿色，5位蓝色）的图像，无alpha。
+
+- `TYPE_BYTE_GRAY` = 10
+
+	表示无符号字节灰度图像，无索引。
+
+- `TYPE_USHORT_GRAY` = 11
+
+	表示无符号短灰度图像，非索引）。
+	
+- `TYPE_BYTE_BINARY` = 12 
+
+	表示不透明的字节打包的1,2或4位图像。
+	
+- `TYPE_BYTE_INDEXED` = 13
+
+	表示索引的字节图像。
+ 
+## 使用事例
+
+#### 查看帮助
+
+	➜  java -jar CheckAndroidV2Signature.jar
+
+	usage: java -jar ./CheckAndroidV2Signature.jar [--version] [--help] [filePath]
+	
+	such as:
+	
+		 java -jar ./CheckAndroidV2Signature.jar --version
+		 java -jar ./CheckAndroidV2Signature.jar --help
+		 java -jar ./CheckAndroidV2Signature.jar ./test.apk
+	
+	after check,the result will be a string json such as:
+	
+		 {"ret":0,"msg":"ok","isV2":true,"isV2OK":true}
+	
+		 ret: result code for check
+	
+			 0 : command exec succ
+			 -1 : file not found
+			 -2 : file not an Android APK file
+			 -3 : check File signature error ,retry again
+	
+		 msg: result msg for check
+		 isV2: whether the file is use Android-V2 signature or not
+		 isV2OK: whether the file's Android-V2 signature is ok or not
+			
+			
+#### 查看版本
+
+	➜  java -jar ./CheckAndroidV2Signature.jar --version
+		com.tencent.ysdk.CheckAndroidV2Signature version 1.0.1 (CheckAndroidV2Signature - 2)
+		homepage : https://github.com/bihe0832/AndroidGetAPKInfo
+		blog : http://blog.bihe0832.com
+		github : https://github.com/bihe0832
+		
+#### 查看应用信息
+
+	➜  java -jar ./CheckAndroidV2Signature.jar ./YSDK_Android_1.3.1_629-debug-ysdktest-inner.apk
+	{"ret":0,"msg":"ok","isV2":false,"isV2OK":false}
+		
+
---
+++ src/com/bihe0832/png/getPNGInfo.java
@@ -0,0 +1,96 @@
+package com.bihe0832.png;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import javax.imageio.ImageIO;
+
+public class getPNGInfo {
+
+	private static final int VERSION_CODE = 1;
+	private static final String VERSION_NAME = "1.0.0";
+	private static final String HELP_PAGE_GENERAL = "help.txt";
+	private static final String VERSION_PAGE_GENERAL = "help_version.txt";
+	
+	private static final int RET_SUCC = 0;
+	private static final int RET_FILE_NOT_EXIST = -1;
+	private static final int RET_GET_INFO_FAILED = -2;
+	
+	public static final String KEY_RESULT_RET = "ret";
+	public static final String KEY_RESULT_MSG = "msg";
+	public static final String KEY_RESULT_HAS_ALPHA = "hasAlpha";
+	public static final String KEY_RESULT_IMG_TYPE = "type";
+	public static final String KEY_RESULT_IMG_WIDTH = "width";
+	public static final String KEY_RESULT_IMG_HEIGHT = "height";
+	
+	public static void main(String[] params) throws Exception {
+        if ((params.length == 0)) {
+            printUsage(HELP_PAGE_GENERAL);
+            return;
+        }
+        if (params[0].toLowerCase().startsWith("--help")) {
+            printUsage(HELP_PAGE_GENERAL);
+            return;
+        } else if (params[0].toLowerCase().startsWith("--version")) {
+    		System.out.println(getPNGInfo.class.toString() + " version " + VERSION_NAME + " (" + VERSION_CODE + ")\n");
+    		printUsage(VERSION_PAGE_GENERAL);
+            return;
+        } else{
+        	getPNGInfoByPath(params[0]);
+			return;
+		}
+    } 
+	
+	public static void getPNGInfoByPath(String filepath){
+		File tempImg = new File(filepath);
+		if(tempImg.exists()){
+			try {
+				BufferedImage img = ImageIO.read(tempImg);
+				System.out.println("{\""+ KEY_RESULT_RET +"\":" + RET_SUCC +
+										",\""+ KEY_RESULT_MSG +"\":\"" +  
+											"图片是否有渐变: " + img.getColorModel().hasAlpha() + " ,"+  
+											"图片尺寸为(宽*高): " + img.getWidth() + " * " +  img.getHeight() + " , " +  
+											"图片类型: " + img.getType() + "\"" +
+										",\""+ KEY_RESULT_HAS_ALPHA +"\":" +  img.getColorModel().hasAlpha() + 
+										",\""+ KEY_RESULT_IMG_TYPE +"\":" +  img.getType() + 
+										",\""+ KEY_RESULT_IMG_WIDTH +"\":" +  img.getWidth() + 
+										",\""+ KEY_RESULT_IMG_HEIGHT +"\":" +  img.getHeight() + 
+								"}\n"); 
+			} catch (Exception e) {
+				printFailedCheckResult(RET_GET_INFO_FAILED," get file :" + filepath +" info failed");
+			}
+		}else{
+			printFailedCheckResult(RET_FILE_NOT_EXIST," file :" + filepath +" is not exist");
+		}
+	}
+	
+	private static void printFailedCheckResult(int ret,String Msg){
+		System.out.println("{\""+ KEY_RESULT_RET +"\":" + ret + ",\""+ KEY_RESULT_MSG +"\":\"" + Msg + "\"}\n"); 
+	}
+	
+	private static void printUsage(String page) {
+        try (BufferedReader in =
+                new BufferedReader(
+                        new InputStreamReader(
+                                getPNGInfo.class.getResourceAsStream(page),
+                                StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read " + page + " resource");
+        }
+    }
+
+    private static class ParameterException extends Exception {
+        private static final long serialVersionUID = 1L;
+        ParameterException(String message) {
+            super(message);
+        }
+    }
+}
